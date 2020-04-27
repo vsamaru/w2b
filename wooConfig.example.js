422ed57a6670e6359036fa2191238296bd883a34
vsamaru <vsamaru@gmail.com>
2020-04-28 02:42:13 +0300

    Update wooConfig.example.js

--- wooConfig.example.js
+++ wooConfig.example.js
@@ -1,6 +1,6 @@
 const wooConfig = {
-	siteUrl: 'xxxxx', // e.g. 'http://xyz.com' - Your WordPress site URL.
-	graphqlUrl: 'xxxxx' // e.g. 'http://wordpressSiteUrl.com/graphql' - Your WordPress site url followed by '/graphql'
+	siteUrl: 'http://w2b.now.sh', // e.g. 'http://xyz.com' - Your WordPress site URL.
+	graphqlUrl: 'http://w2b.now.sh/graphql' // e.g. 'http://wordpressSiteUrl.com/graphql' - Your WordPress site url followed by '/graphql'
 };
 
 module.exports = wooConfig;
