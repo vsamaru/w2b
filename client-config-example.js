6a3e83636175d8025cb1df4a5356831d89059cc5
vsamaru <vsamaru@gmail.com>
2020-04-28 02:41:31 +0300

    Update and rename client-config-example.js to client-config.js

--- client-config-example.js
+++
@@ -1,6 +0,0 @@
-const clientConfig = {
-	siteUrl: 'xxxxx', // e.g. 'http://xyz.com' - Your WordPress site URL.
-	graphqlUrl: 'xxxxx' // e.g. 'http://wordpressSiteUrl.com/graphql' - Your WordPress site url followed by '/graphql'
-};
-
-export default clientConfig;
---
+++ client-config.js
@@ -0,0 +1,6 @@
+const clientConfig = {
+	siteUrl: 'http://w2b.now.sh', // e.g. 'http://w2b.now.sh' - Your WordPress site URL.
+	graphqlUrl: 'http://w2b.now.sh/graphql' // e.g. 'http://w2b.now.sh/graphql' - Your WordPress site url followed by '/graphql'
+};
+
+export default clientConfig;
